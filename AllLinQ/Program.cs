36879bc6028397e034b856a12fc248d2ca93a68b
George-Amin <TUF@LAPTOP-J24NQ5H9>
2025-12-22 13:59:42 +0200

    Add project files.

---
+++ AllLinQ/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace AllLinQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            #region LINQ Syntax [fluent , query] Syntax
+            #region fluent Syntax
+            // Fluent Syntax
+            //// 1.can call LinQ Operators as a static methods =>
+            //var Result = Enumerable.Where(Numbers, nums => nums % 2 == 0);
+
+
+            //// 2.calling as Extensions methods .
+            //// Extensions methods [Recommended]
+
+            //var Result = Numbers.Where(nums => nums % 2 == 0);
+
+            #endregion
+
+            #region Query Syntax
+
+         /*   var Result = from nums in Numbers
+                        where nums % 2 == 0
+                        select nums;
+*/
+
+           
+
+            #endregion
+
+/*
+            foreach (var item in Result)
+            {
+                Console.WriteLine(item);
+            }
+*/
+            #endregion
+        }
+    }
+}
