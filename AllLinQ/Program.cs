b6e87f28ef77e0fddd1877bc87f3b34910f04881
George-Amin <TUF@LAPTOP-J24NQ5H9>
2025-12-24 10:31:46 +0200

    Part 09 Transformation Operators - Select , Select Many

--- AllLinQ/Program.cs
+++ AllLinQ/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace AllLinQ
+﻿using AllLinQ.Data;
+using System.Runtime.CompilerServices;
+using static AllLinQ.ListGenerator;
+
+namespace AllLinQ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             #region LINQ Syntax [fluent , query] Syntax
             #region fluent Syntax
             // Fluent Syntax
@@ -53,21 +57,177 @@
 
             #region Immediate Execution
 
-            //// use casting operator or aggregate or elemenet
-            
-            var GetOddNums = Numbers.Where(nums => nums % 2 == 1).ToList();
+            //// use casting operators or aggregate operators or elemenets operators
 
-            Numbers.AddRange(new int[] { 11, 12, 13, 14, 15 });
+            /*     var GetOddNums = Numbers.Where(nums => nums % 2 == 1).ToList();
 
-            foreach (int num in GetOddNums)
+                 Numbers.AddRange(new int[] { 11, 12, 13, 14, 15 });
+
+                 foreach (int num in GetOddNums)
+                 {
+                     Console.WriteLine(num);
+                 }
+     */
+
+            #endregion
+
+            #endregion
+
+
+
+            #region Local 
+
+            //var Result = ListGenerator.CustomersList.Select(c => c);
+            //foreach (var c in Result)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            //Console.WriteLine(ListGenerator.ProductList[0]);
+            //Console.WriteLine(ListGenerator.CustomersList[0]);
+
+            // The ListGenerator is static class that can be using 
+            //Console.WriteLine(ProductList[0]);
+            //Console.WriteLine(CustomersList[0]);
+
+            #endregion
+            #region Filtration - Where 
+            //// fluent syntax
+            //var Result = ProductList.Where(c => c.ProductID == 22);
+            ////Query syntax
+            //var Result = from Pro in ProductList
+            //             where Pro.ProductID == 22
+            //             select Pro;
+
+
+            //var Result = ProductList.Where(pro => pro.UnitsInStock <= 0);
+            //   var Result = ProductList.Where(pro => pro.UnitsInStock > 0 && pro.Category == "Meat/Poultry");
+
+            ////Query syntax
+            ///
+            //var Result = from pro in ProductList
+            //             where pro.UnitsInStock > 0 && pro.Category == "Meat/Poultry"
+            //             select pro;
+
+            #region indexed where
+
+            //var Result = ProductList.Where((p, i) => p.UnitsInStock == 0 && i < 10);
+            //var Result = ProductList.Where((p, i) => i < 10 && p.UnitsInStock == 0);
+
+            #endregion
+            #endregion
+
+            #region Part 09 Transformation Operators - Select , Select Many
+            ////Fluent syntax
+            //var Result = ProductList.Select(p => p.ProductName);
+
+            //// Query syntax
+            //var Result = from pro in ProductList
+            //             select pro.ProductName;
+            #region selectMany()
+
+            ////use selectMany() with collections of something
+            //var Result = CustomersList.SelectMany(c => c.Orders);
+
+
+            //// query syntax
+            //var Result = from c in CustomersList
+            //             from order in c.Orders
+            //             select order;
+
+
+            #endregion
+            #region select() with  anonymous  object
+
+            // return anonymous object use [ new ]  
+            ////.. the CLR will create new class in runtime and override on ToString()
+            //var Result = ProductList.Select(pr => new { pr.ProductID , pr.ProductName});
+
+            ////query syntax 
+            ///if use the same var should use the same names
+
+            //Result = from pr in ProductList
+            //            select new
+            //            {
+            //                ProductID = pr.ProductID,
+            //                ProductName = pr.ProductName,
+            //            };
+            #endregion
+
+
+            #region Q1 select product UnitsInStock and make discount on UnitPrice 10%
+
+            /*
+             ////fluent syntax
+                        var Result = ProductList.Where(p => p.UnitsInStock > 0)
+                            .Select(dis => new {
+                                ProductId = dis.ProductID,
+                                ProductName = dis.ProductName,
+                                OldPrice = dis.UnitPrice,
+                                NewPrice = dis.UnitPrice - (dis.UnitPrice * 0.1m),
+                                discount   = dis.UnitPrice * 0.1m
+                            });
+            */
+
+            //// query syntax
+            /*
+                        var Result = from p in ProductList
+                                     where p.UnitsInStock > 0
+                                     select new
+                                     {
+                                         p.ProductID,
+                                         p.ProductName,
+                                         p.UnitPrice,
+                                        OldPrice =  p.UnitPrice - (p.UnitPrice * 0.1m),
+                                     };
+            */
+            #endregion
+
+            #region Select overload return index with ..(indexed select is valid only with fluent syntax)
+            //// indexed select is valid only with fluent syntax
+            //// var Result = ProductList.Where(p => p.UnitsInStock > 0).Select((p, i) => new { Index = i, p.ProductName });
+
+            #endregion
+
+            #endregion
+
+
+            #region Part 10 Ordering Operators
+
+            //var Result = ProductList.OrderBy(p => p.UnitPrice); //// Ascending order   
+            //var Result = ProductList.OrderByDescending(p => p.UnitPrice); //// Descending order   
+
+            /*
+            var Result = from p in ProductList
+                         orderby p.UnitPrice  //// Ascending order  
+                         select p;
+*/
+            /*
+                        var Result = from p in ProductList
+                                     orderby p.UnitPrice descending //// Ascending order  
+                                     select p;
+            */
+
+            #region make to ordering
+            //var Result = ProductList.OrderBy(p => p.UnitPrice).ThenBy(p => p.UnitsInStock);
+            //var Result = ProductList.OrderBy(p => p.UnitPrice).ThenByDescending(p => p.UnitsInStock);//// ThenByDescending() order by then order by 
+
+            // Reverse()
+            //var Result = ProductList.Where(p => p.UnitsInStock > 0).Reverse();
+            #endregion
+
+            #endregion
+
+
+     
+
+            //Console.WriteLine(Result);
+
+            foreach (var i in Result)
             {
-                Console.WriteLine(num);
+                Console.WriteLine(i);
             }
 
-
-            #endregion
-
-            #endregion
         }
     }
 }
